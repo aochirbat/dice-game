f9d6d02a349928f2f3c4d48d2574df9c87e70c49
ochirbat <ochkiwork@gmail.com>
2020-05-17 15:58:07 +0800

    onoonii oorcloltig bii bolgoj eeljiig soliw

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 
 
 //Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
@@ -19,4 +19,18 @@ document.querySelector('.btn-roll').addEventListener('click', function (){
     var dice = Math.floor(Math.random() * 6)+1;
     document.querySelector('.dice').style.display = 'block';
     document.querySelector('.dice').src = "dice-" + dice + '.png'; 
+    if(dice !== 1){
+        roundScore =roundScore + dice;
+        document.getElementById('score-' + activePlayer).textContent = roundScore;     
+    }else {
+        roundScore = 0;
+        document.getElementById('score-0').textContent = 0;
+
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+    }
+    
+   
 });
