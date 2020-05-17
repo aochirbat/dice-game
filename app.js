0a9972253a8732725fcb4d6b6fc8a3b5d587cc3f
ochirbat <ochkiwork@gmail.com>
2020-05-17 13:43:02 +0800

    Shoonii dugaaraar zurgiin solison oorclilt

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var scores = [ 0, 0]
 var roundScore = 0;
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг Random-оор хийж өгнө.
-var dice = Math.floor(Math.random() * 6)+1;
-document.querySelector('#score-0').textContent = dice;
-console.log('shoo: ' + dice)
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    var dice = Math.floor(Math.random() * 6)+1;
+    document.querySelector('.dice').style.display = 'block';
+    document.querySelector('.dice').src = "dice-" + dice + '.png'; 
+});
