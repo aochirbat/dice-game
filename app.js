1e6512d8ad78e475b28317a61f136ca3475b7edf
ochirbat <ochkiwork@gmail.com>
2020-05-18 14:49:26 +0800

    belen bolson bugd

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-//Хэдэн оноогоор тоглох.
+var activePlayer;
+var scores ;
+var roundScore;
+var diceNone = document.querySelector('.dice');
+newGame();
+function newGame (){
+    //Хэдэн оноогоор тоглох.
 // var limitScore = prompt('Хэдэн оноогоор тоглох вэ?');
 
 //Тоглогчийн ээлжийг хадгалах хувьсагч
@@ -6,24 +12,34 @@ var activePlayer = 0;
 
 
 //Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
-var scores = [ 0, 0]
+var scores = [0, 0]
 
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    diceNone.style.display = 'none';
+}
 
 //Шооны аль талаараа буусныг хадгалах хувьсагч 1-6 гэсэн утгыг Random-оор хийж өгнө.
 
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
+
 function switchPlayer(){
         roundScore = 0;
         document.getElementById('score-'+ activePlayer).textContent = 0;
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        diceNone.style.display = 'none';
 }
 
 document.querySelector('.btn-roll').addEventListener('click', function (){
@@ -45,17 +61,23 @@ document.querySelector('.btn-roll').addEventListener('click', function (){
 //Hold process 
 
 document.querySelector('.btn-hold').addEventListener('click' , function(){
-  
+       
     //Тоглогчын цугуулсан оноог хадгалах хэсэг рүү хийх.
        scores[activePlayer] = scores[activePlayer] + roundScore;
        document.getElementById('current-' + activePlayer).textContent = scores[activePlayer];
        if(scores[activePlayer] >= 10 ){
         document.getElementById('name-' + activePlayer).textContent ='Winner!!!';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+        diceNone.style.display = 'none';
         } else{
             switchPlayer();
         }
+       
+        
       
     //Rounscore 0 болгож өгнө.
     //Ээлжийг нь сольж өгнө.
     
 });
+document.querySelector('.btn-new').addEventListener('click', newGame);
