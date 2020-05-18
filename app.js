71c4d6271670fc49694198f54b9461e44af19e46
ochirbat <ochkiwork@gmail.com>
2020-05-18 16:32:06 +0800

    Duussan 100%

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var activePlayer;
 var scores; 
 var roundScore;
+var isNewGame;
 newGame();
 var diceNone = document.querySelector('.dice');
 function newGame(){
     //Хэдэн оноогоор тоглох.
-// var limitScore = prompt('Хэдэн оноогоор тоглох вэ?');
-//Тоглогчийн ээлжийг хадгалах хувьсагч
-activePlayer = 0;
-//Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
-scores = [0, 0];
-//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-roundScore = 0;
+    // var limitScore = prompt('Хэдэн оноогоор тоглох вэ?');
+    isNewGame = true;
+    //Тоглогчийн ээлжийг хадгалах хувьсагч
+    activePlayer = 0;
+    //Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+    scores = [0, 0];
+    //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+    roundScore = 0;
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
@@ -34,28 +36,37 @@ function switchPlayer(){
         diceNone.style.display = 'none';
 };
 document.querySelector('.btn-roll').addEventListener('click', function (){
-    var dice = Math.floor(Math.random() * 6)+1;
-    document.querySelector('.dice').style.display = 'block';
-    document.querySelector('.dice').src = "dice-" + dice + '.png'; 
-    if(dice !== 1){
-        roundScore =roundScore + dice;
-        document.getElementById('score-' + activePlayer).textContent = roundScore;     
+    if(isNewGame){
+        var dice = Math.floor(Math.random() * 6)+1;
+        document.querySelector('.dice').style.display = 'block';
+        document.querySelector('.dice').src = "dice-" + dice + '.png'; 
+        if(dice !== 1){
+            roundScore =roundScore + dice;
+            document.getElementById('score-' + activePlayer).textContent = roundScore;     
+        }else {
+            switchPlayer();
+    }
     }else {
-        switchPlayer();
+        alert('Game over');
     }
 });
 document.querySelector('.btn-hold').addEventListener('click' , function(){
-       
+    if(isNewGame){   
     //Тоглогчын цугуулсан оноог хадгалах хэсэг рүү хийх.
        scores[activePlayer] = scores[activePlayer] + roundScore;
        document.getElementById('current-' + activePlayer).textContent = scores[activePlayer];
        if(scores[activePlayer] >= 10 ){
+           isNewGame = false;
         document.getElementById('name-' + activePlayer).textContent ='Winner!!!';
         document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
         document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
         diceNone.style.display = 'none';
+        alert('Game over');
         } else{
             switchPlayer();
-        }    
+        }
+    } else{
+        alert('Game over');
+    }
 });
 document.querySelector('.btn-new').addEventListener('click', newGame);
